e9a1073e7502a10723a22f1fba3d284dc24ef611
Lorenzo-Tr <endzz2209@gmail.com>
2023-01-27 14:00:24 +0100

    Add board to Piece

--- include/Piece.hpp
+++ include/Piece.hpp
@@ -1,13 +1,19 @@
 #pragma once
 
+#include "ChessBoard.hpp"
+
 class Piece {
  private:
   bool color_;
   int x_;
   int y_;
+  ChessBoard* board_;
 
  public:
-  Piece(int x, int y, bool color);
+  Piece(int x, int y, bool color, ChessBoard* board);
+  inline bool getColor() { return color_; }
+  inline ChessBoard* getBoard() { return board_; }
+  inline void setBoard(ChessBoard* board) { board_ = board; }
 
   // Check if is a valid move
   virtual bool validate_move(int x, int y) = 0;
@@ -17,45 +23,45 @@ class Piece {
 
 class King : public Piece {
  public:
-  King(int x, int y, bool color);
+  King(int x, int y, bool color, ChessBoard* board);
   bool validate_move(int x, int y);
   void move(int x, int y);
 };
 
 class Queen : public Piece {
  public:
-  Queen(int x, int y, bool color);
+  Queen(int x, int y, bool color, ChessBoard* board);
   bool validate_move(int x, int y);
   void move(int x, int y);
 };
 
 class Rook : public Piece {
  public:
-  Rook(int x, int y, bool color);
+  Rook(int x, int y, bool color, ChessBoard* board);
   bool validate_move(int x, int y);
   void move(int x, int y);
 };
 
 class Knight : public Piece {
  public:
-  Knight(int x, int y, bool color);
+  Knight(int x, int y, bool color, ChessBoard* board);
   bool validate_move(int x, int y);
   void move(int x, int y);
 };
 
 class Bishop : public Piece {
  public:
-  Bishop(int x, int y, bool color);
+  Bishop(int x, int y, bool color, ChessBoard* board);
   bool validate_move(int x, int y);
   void move(int x, int y);
 };
 
 class Pawn : public Piece {
  private:
-  bool can_move_;
+  bool already_move_;
 
  public:
-  Pawn(int x, int y, bool color, bool can_move);
+  Pawn(int x, int y, bool color, bool already_move, ChessBoard* board);
   bool validate_move(int x, int y);
   void move(int x, int y);
   void forward();
