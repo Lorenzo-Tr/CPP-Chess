0af4c5740d3203304e111b5590dff5ea26a71ea3
Lorenzo-Tr <endzz2209@gmail.com>
2023-01-27 11:33:43 +0100

    Validate Move King

--- include/Piece.hpp
+++ include/Piece.hpp
@@ -8,6 +8,8 @@ class Piece {
 
  public:
   Piece(int x, int y, bool color);
+  inline int getX() { return x_; }
+  inline int getY() { return y_; }
 
   // Check if is a valid move
   virtual bool validate_move(int x, int y) = 0;
